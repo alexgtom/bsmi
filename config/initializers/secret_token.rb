b876a91ec27ac3d3c3a7c0b1c0430769cf5f853f
Alex Tom <alexgtom@gmail.com>
2012-10-01 20:44:22 -0700

    Started initial ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsmi::Application.config.secret_token = '268ab8b9717f931564ce442e6195919cce2ae6c034589cf9a6c987da93f257fe82fe683dba143c524b4cfffe7d5e5ab92f85b0a1a3454a780245a645180533fc'

bb4583d9adf50b13f55e718da6f15acabd8f5f99
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-14 16:39:32 -0700

    Made preference validate uniqueness of student_id, timeslot_id pairs

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Preference do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to :student}
+  it {should belong_to :timeslot}
+
+  it {should validate_uniqueness_of(:student_id).scoped_to(:timeslot_id)}
 end

05ad864c96095f59c92b072851abb1fb1722514b
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-14 16:44:20 -0700

    Added has_many relationships to timeslot

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,5 @@
 class Timeslot < ActiveRecord::Base
-  # attr_accessible :title, :body
+  has_many :preferences
+  has_many :students, :through => :preferences
   
 end
--- spec/models/timeslot_spec.rb
+++ spec/models/timeslot_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Timeslot do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many :preferences}
+  it {should have_many(:students).through(:preferences)}
 end

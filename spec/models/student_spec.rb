fc0c72e4939a05ab04aa90be250f69024c1e5956
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-14 16:19:46 -0700

    Fixed student spec relationship tests

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe Student do
   it {should have_many :preferences}
-  it {should have_many :timeslots, :through => :preferences}
+  it {should have_many(:timeslots).through(:preferences)}
 end

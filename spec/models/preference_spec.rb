15a6718a278bd8703377b9eed98eeb1cb3f7d1d5
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-14 18:30:57 -0700

    Added validation spec to preference_spec as TODO

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -5,4 +5,7 @@ describe Preference do
   it {should belong_to :timeslot}
 
   it {should validate_uniqueness_of(:student_id).scoped_to(:timeslot_id)}
+  
+  it {should validate_presence_of :student}
+  it {should validate_presence_of :timeslot}
 end

e4b8769a8f993f3419f726c92bfabc80744c7235
Alex Tom <alexgtom@gmail.com>
2012-10-16 15:06:02 -0700

    Moved css/js in timeslot selection to head

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,12 @@ Bsmi::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.dev_tweaks.autoload_rules do
+    keep :all
+
+    skip '/favicon.ico'
+    skip :assets
+    skip :xhr
+    keep :forced
+  end
 end

e6cfa72126e411659cf24ba898a59381fd3f293e
Alex Tom <alexgtom@gmail.com>
2012-10-19 14:25:53 -0700

    Removed unneeded code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -80,28 +80,4 @@ class StudentsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  def timeslot_selection
-    flash[:notice] = params
-
-    student_id = params[:id]
-    
-    Preference.destroy_all(:student_id => student_id)
-
-    Timeslot.day_list.each do |d|
-      if params[d]
-
-        # create or update new preferences
-        params[d].each do |timeslot_id|
-          p = Preference.create(
-            :student_id => student_id,
-            :timeslot_id => timeslot_id,
-            :updated_at => DateTime.now,
-          )
-        end
-
-      end
-    end
-  end
-
 end

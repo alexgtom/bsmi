c2809eb0bdbc664805cf2dabeb2eb3683054893f
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-21 20:14:09 -0700

    Added weekdays method to timeslots

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,8 +1,12 @@
 class Timeslot < ActiveRecord::Base
   @@DAY = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
-
+  @@WEEK_DAYS = @@DAY - [:sunday, :saturday]
   def self.day_list
     @@DAY
+  end
+
+  def self.weekdays
+    @@WEEK_DAYS
   end
 
   def self.day_index(value)
@@ -16,7 +20,9 @@ class Timeslot < ActiveRecord::Base
   def day=(value)
     write_attribute(:day, @@DAY.index(value))
   end
-  
+
+
+
   attr_protected #none
   has_many :preferences
   has_many :students, :through => :preferences

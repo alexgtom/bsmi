0103cda329432353ab7ee1a98168707bc4c64f60
orionzhao <orionzhao91@gmail.com>
2012-10-21 21:34:44 -0700

    modified controller

--- app/controllers/mentor_teacher/schedules_controller.rb
+++ app/controllers/mentor_teacher/schedules_controller.rb
@@ -3,34 +3,38 @@
 class MentorTeacher::SchedulesController < ApplicationController
     #MentorTeacher has many timeslots
     
-=begin   
+   
   def new
     #timeslots == JSON.parser(params[:timeslots])
-    params[:timeslots].each do |hash|
-      @timeslot = Timeslot.new(hash)
-      cur_teacher.timeslots << @timeslot
+    # @schedule = []
+    if current_user
+      params[:timeslots].each do |hash|
+        @timeslot == Timeslot.new(hash)
+        current_user.timeslots << @timeslot
+      end
+      current_user.save
     end
   end
-=end
 
   def show
-    id = params[] # retrieve movie ID from URI route
-    @timeslot = Timeslot.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    if current_user
+       @timeslot = current_user.timeslots
+    end
   end
   
   def create
-
+   if current_user
     params[:timeslots].each do |hash|
       @timeslot = Timeslot.create!(hash)
-      cur_teacher.timeslots << @timeslot
+      current_user.timeslots << @timeslot
     end
     flash[:notice] = "Schedule was successfully created."
     # redirect_to mentor_teacher_schedule_path
+   end
   end
 
   def edit
-    @timeslot = Timeslot.find(params[:id])
+   # @timeslot = Timeslot.find(params[:id])
   end
 
   def update
@@ -45,10 +49,11 @@ class MentorTeacher::SchedulesController < ApplicationController
   end
 
   def destroy
+=begin
     @timeslot = Timeslot.find(params[:id])
     @timeslot.destroy
     flash[:notice] = "Schedule deleted."
    # redirect_to mentor_teacher_schedule_path
+=end
   end
-
 end

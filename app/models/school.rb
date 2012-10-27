e7c002a9b1ce35bf6dc486a3bfb6f56095432d74
Alex Tom <alexgtom@gmail.com>
2012-10-26 17:22:46 -0700

    Added school scafolding

---
+++ app/models/school.rb
@@ -0,0 +1,20 @@
+class School < ActiveRecord::Base
+  @@LEVEL = [:high, :middle, :elementary]
+  attr_accessible :name
+
+  def self.level_list
+    @@DAY
+  end
+
+  def self.level_index(value)
+    @@DAY.index(value)
+  end
+
+  def level
+    @@LEVEL[read_attribute(:day)]
+  end
+
+  def level=(value)
+    write_attribute(:level, @@level.index(value))
+  end
+end
---
+++ spec/views/schools/edit.html.haml_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "schools/edit" do
+  before(:each) do
+    @school = assign(:school, stub_model(School,
+      :name => "MyString",
+      :level => "MyString"
+    ))
+  end
+
+  it "renders the edit school form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => schools_path(@school), :method => "post" do
+      assert_select "input#school_name", :name => "school[name]"
+      assert_select "input#school_level", :name => "school[level]"
+    end
+  end
+end

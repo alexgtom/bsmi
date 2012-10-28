58126e1a7a039cc70b69cb967c1815a5af790ca4
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-28 11:34:31 -0700

    Got rid of bootstrap require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,8 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery.weekcalendar
-//= require bootstrap
-
 //= require_tree .

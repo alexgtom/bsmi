9dcb07437f3ad0550c6f45a16e72711384de8903
Andrew Mains <andrewmains12@berkeley.edu>
2012-10-28 11:47:29 -0700

    Added validations to timeslot, functions to bring it to and from cal_event json

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,16 @@
 class Timeslot < ActiveRecord::Base
+  attr_protected #none
+
+  #Associations
+  has_many :preferences
+  has_many :students, :through => :preferences
+  belongs_to :mentor_teacher
+
+  #Validations
+  validates :day, :presence => true
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
+
   @@DAY = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
   @@WEEK_DAYS = @@DAY - [:sunday, :saturday]
   def self.day_list
@@ -14,7 +26,8 @@ class Timeslot < ActiveRecord::Base
   end
 
   def day
-    @@DAY[read_attribute(:day)]
+    index = read_attribute(:day)
+    @@DAY[index] unless index.nil?
   end
 
   def day=(value)
@@ -27,13 +40,22 @@ class Timeslot < ActiveRecord::Base
              :start_time => Time.parse(event["start"]),
              :end_time => Time.parse(event["end"]))    
   end
-  
-  attr_protected #none
-  has_many :preferences
-  has_many :students, :through => :preferences
-  belongs_to :mentor_teacher
 
+  #Convert this timeslot into something understood by the front end
+  def to_cal_event_hash
+    time_format = '%Y/%m/%d %H:%M:%S %z'
+    { 'id' => self.id,
+      'start' => 'new Date(#{self.start_time.strftime(time_format)})',
+      'end' => 'new Date(#{self.end_time.strftime(time_format)})',
+      'title' => self.class_name
+    }
+  end
+  
   def selected?(student_id)
     Preference.where(["student_id = ?", student_id]).where(:timeslot_id => id).size > 0
   end
+
+
+
+  
 end

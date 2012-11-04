5e96eba08e01ca233540590f1afc412509fc22da
orionzhao <orionzhao91@gmail.com>
2012-11-03 20:22:57 -0700

    not much

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,22 @@
 class StudentsController < ApplicationController
   def index
     @all_student = User.where(:owner_type => "Student")
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'name'
-       @all_student = @all_student.order(:name)
-    when 'course'
-       @all_student = @all_student.order(:course)
+    if params[:sort] || session[:sort] != nil
+      sort = params[:sort] || session[:sort]
+      case sort
+      when 'name'
+         @all_student = @all_student.order(:name)
+      when 'course'
+         @all_student = @all_student.order(:placement)
+      end
     end
     if params[:search] || session[:search] != nil
       search = params[:search] || session[:search]
       search_condition = "%" + search + "%"
       @all_student = @all_student.find(:all, :conditions => ['name LIKE ?', search_condition])
     end   
-      
+  end
   def placements
   end
 end
+

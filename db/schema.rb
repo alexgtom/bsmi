02d762b83e626c647f19c0b7406bc1ba2de2d89d
Andrew Mains <andrewmains12@berkeley.edu>
2012-11-04 18:50:43 -0800

    Removed extraneous columns from mentor_teachers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103235450) do
+ActiveRecord::Schema.define(:version => 20121104181650) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -27,12 +27,8 @@ ActiveRecord::Schema.define(:version => 20121103235450) do
   end
 
   create_table "mentor_teachers", :force => true do |t|
-    t.string   "mailing_address"
-    t.string   "phone_number"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "school_id"
   end
 

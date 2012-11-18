ec5b84c25951c3282496193abd7383b0b0d3d6b3
Andrew Mains <andrewmains12@berkeley.edu>
2012-11-18 03:10:49 -0800

    Specced out some helpers for graph normalization

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -1,27 +1,112 @@
 require 'rglpk'
+require 'set'
+
+class Graph
+  attr_accessor :adjacency_list
+
+  def initialize(preferences=nil)
+    self.adjacency_list = Hash.new {|hash, key| hash[key] = Hash.new}
+
+    if preferences
+      preferences.each do |p| 
+        self.add_edge(p.student_id, p.timeslot_id, p.ranking)
+      end
+    end
+  end
+
+  def add_edge(s, t, weight=1)
+    self.adjacency_list[s][t] = weight
+  end
+
+  def connected?(s, t)
+    return self.adjacency_list[s].include? t
+  end
+
+  def edge_weight(s,t)
+    return self.adjacency_list[s][t]
+  end
+end
 
 class MatchingSolver
   attr_reader :preferences
   def initialize(preferences)
     @preferences = preferences
+    @students = Set.new(preferences.map {|p| p.student_id}).to_a
+    @timeslots = Set.new(preferences.map {|p| p.timeslot_id}).to_a
   end
 
   def solve
-    problem = MatchingProblem.new(preferences)
+    self.normalize_graph
+    problem = MatchingProblem.new(preferences, students, timeslots)
     return problem.solution
   end
 
+  #Problem: not enough teachers for students
+  #Solution: make dummy nodes for timeslots that can accomodate more
+  private
+
+  def normalize_graph
+    self.expand_timeslots
+
+    self.equalize_graph_sides
+    self.connect_graph
+    #Make sure everyone has an edge to everyone else
+    
+    
+    #Goals
+    #Equal numbers of teachers and students
+    #Each timeslot duplicated up to max num students
+    #Give everyone an edge to everything
+    #If pick dummy edge for student, not actually matched
+
+    #3 classes of dummy:
+    # Dummy student to real timeslot = don't use timeslot
+    # Real student to dummy timeslot = student is unmatched
+    # Real student, real timeslot, dummy edge = student is unmatched
+
+    # More students than timeslots:
+    #   -> dummy timeslots
+
+    # More timeslots than students:
+    #   -> dummy students
+    
+    # Can't have both
+  end
+
+  def expand_timeslots
+    
+  end
+
+  def equalize_graph_sides
+
+  end
+
+  DUMMY_EDGE_WEIGHT = 1000
+
+  def connect_graph
+    self.students.each do |s|
+      self.timeslots.each do |t|
+        if not self.graph.connected?(s,t)
+          self.graph.add_edge(s,t, DUMMY_EDGE_WEIGHT)
+        end
+      end
+    end
+  end
+
+  def extract_solution(matchings)
+    
+  end
   #Represents an assignment problem instance between mentor teacher timeslots
   #and students. 
   class MatchingProblem < Rglpk::Problem
     
     attr_reader :students, :timeslots, :preferences
 
-    def initialize(preferences)           
+    def initialize(preferences, students, timeslots)           
       super()
       @preferences = preferences
-      @students = Set.new(preferences.map {|p| p.student_id}).to_a
-      @timeslots = Set.new(preferences.map {|p| p.timeslot_id}).to_a
+      @students = students
+      @timeslots = timeslots
     end
     
     def prepare_problem
@@ -31,11 +116,6 @@ class MatchingSolver
     end
 
     def solution
-      #Use cached solution if possible
-      # if @solution
-      #   return @solution
-      # end
-#      require 'debugger'; debugger
       self.prepare_problem
       self.simplex
       @solution = self.preferences.zip(self.cols).find_all{|p, col| col.get_prim > 0 }.

fdf3f03011dd37a2677af9158ebb822e575c5897
Alex Tom <alexgtom@gmail.com>
2012-11-18 16:03:36 -0800

    Fixed rspec tests

--- spec/factories/cal_courses.rb
+++ spec/factories/cal_courses.rb
@@ -3,9 +3,7 @@
 FactoryGirl.define do
   factory :cal_course do
     name "MyString"
-    timeslots "1, 2, 3"
-    school_type "MyString"
-    course_grade "MyString"
-    courses  { FactoryGirl.generate(:course) }
+#    timeslots { FactoryGirl.generate(:timeslot) }
+#    courses  { FactoryGirl.generate(:course) }
   end
 end
--- spec/views/mentor_teachers/show.html.haml_spec.rb
+++ spec/views/mentor_teachers/show.html.haml_spec.rb
@@ -4,9 +4,4 @@ describe "mentor_teachers/show" do
   before(:each) do
     @mentor_teacher = assign(:mentor_teacher, stub_model(MentorTeacher))
   end
-
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-  end
 end

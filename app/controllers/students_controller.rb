14556b08bcdf12987fc5ef531b480d86e1e6484e
orionzhao <orionzhao91@gmail.com>
2012-11-20 02:55:07 -0800

    finished tweak feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,6 +17,19 @@ class StudentsController < ApplicationController
   end
 
   def edit_placements
+    if params[:new_timeslot] != nil
+       if Student.find_by_id(params[:id]).placements.find_by_id(params[:new_timeslot]) == nil
+          Student.find_by_id(params[:id]).placements << Timeslot.find_by_id(params[:new_timeslot])
+       else
+       	  redirect_to edit_placements_student_path(params[:id]), :notice => "The student already has the placement you were trying to add."
+       end
+    end
+    if params[:student_id] != nil && params[:timeslot_id] != nil
+      @name = Student.find_by_id(params[:student_id]).user.first_name + Student.find_by_id(params[:student_id]).user.last_name
+      @placements = Student.find_by_id(params[:student_id]).placements
+      @placements.delete(Timeslot.find_by_id(params[:timeslot_id]))
+      redirect_to edit_placements_student_path(params[:student_id]), :notice => "The selected placement has been removed for #{@name}"
+    end
     if Student.find_by_id(params[:id]) == nil
        redirect_to students_path, :notice => "No such a student exists, or student has been removed"
     else

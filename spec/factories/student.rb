0584bf1265c3198de7902e9205092b5c1bee538d
Alex Tom <alexgtom@gmail.com>
2012-11-22 02:11:10 -0800

    Fixed mentoer_teacher and student when associating a user with them

--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     after(:create) do |student, evaluator|
       #Create preferences
       FactoryGirl.build_list(:preference, 5, :student => student)
-      student.user = FactoryGirl.create(:user, :owner_type => "Student", :owner_id => student.id, :owner => student)
+      student.user = FactoryGirl.create(:user, {:owner_type => "Student", :owner_id => student.id, :owner => student})
       end
     end
 

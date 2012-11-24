ed333464c5c22b8f7be6c3d1fc617901b8a5c592
Alex Tom <alexgtom@gmail.com>
2012-11-24 02:18:23 -0800

    Added year field to semester

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121124055228) do
+ActiveRecord::Schema.define(:version => 20121124094817) do
 
   create_table "advisors", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(:version => 20121124055228) do
     t.date     "end_date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "year"
   end
 
   create_table "semesters_students", :id => false, :force => true do |t|

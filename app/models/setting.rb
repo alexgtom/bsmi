478ad80ec74d1b5bb2b86f2bbfe381a94b41753b
Alex Tom <alexgtom@gmail.com>
2012-11-25 15:47:28 -0800

    Added settings mdoel

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,30 @@
-class Setting < RailsSettings::CachedSettings
+class Setting < ActiveRecord::Base
+  attr_accessible :key, :value
+  def self.defaults
+    {
+      'student_min_preferences' => 3, 
+      'student_max_preferences' => 5,
+
+      # for rspec test. dont delete.
+      '!@#$' => 1  
+    }
+  end
+
+  #set a setting value by [] notation
+  def self.[]=(key, value)
+    self.create!(:key => key, :value => value)
+  end
+  
+  #retrieve the actual Setting record
+  def self.[](key)
+    if not self.find_by_key(key)
+      if not self.defaults[key]
+        return nil
+      else
+        return self.defaults[key].to_s
+      end
+    else
+      return self.find_by_key(key).value.to_s
+    end
+  end
 end

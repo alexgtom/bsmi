2b392d603c2b6e76894daaf650e43f340ba489d8
Alex Tom <alexgtom@gmail.com>
2012-11-25 01:22:18 -0800

    Fixed associatiaons which also fixed too many cal_courses being genereted from seeds for some odd reason

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121124103013) do
-
+ActiveRecord::Schema.define(:version => 20121125090622) do
   create_table "advisors", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false

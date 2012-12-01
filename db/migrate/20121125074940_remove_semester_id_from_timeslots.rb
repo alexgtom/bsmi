84d1f2b2443f54473e81681b33c830af47b0f231
Alex Tom <alexgtom@gmail.com>
2012-11-30 23:42:28 -0800

    Removed RemoveSemesterIdFromTimeslots migration

--- db/migrate/20121125074940_remove_semester_id_from_timeslots.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSemesterIdFromTimeslots < ActiveRecord::Migration
-  def up
-    remove_column :timeslots, :semester_id
-  end
-
-  def down
-    add_column :timeslots, :semester_id, :integer
-  end
-end

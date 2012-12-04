88b20fde2435ea1fa063fc8a75bf82121f389966
orionzhao <orionzhao91@gmail.com>
2012-12-03 16:53:33 -0800

    start point

--- db/schema.rb
+++ db/schema.rb
@@ -157,11 +157,15 @@ ActiveRecord::Schema.define(:version => 20121126031903) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "settings", :force => true do |t|
-    t.string   "key"
-    t.string   "value"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "var",                      :null => false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", :limit => 30
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
   end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
 
   create_table "students", :force => true do |t|
     t.datetime "created_at", :null => false

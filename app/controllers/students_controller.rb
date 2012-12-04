2d5aebfc599691980025ff4c2a9bcbc80cdd0618
orionzhao <orionzhao91@gmail.com>
2012-12-03 17:35:10 -0800

    students_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
   before_filter :require_student, :only => [:placements]
+  
   def index
     store_location
     @all_student = User.where(:owner_type => "Student")

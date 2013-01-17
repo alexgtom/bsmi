eb2c9e094b77a90c0c9f9fc4412cf38132994c06
Alex Tom <alexgtom@berkeley.edu>
2013-01-16 19:59:02 -0800

    Added redirect to error path

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -4,6 +4,7 @@ class MatchingsController < ApplicationController
     cur_semester = Semester.current_semester
     if not cur_semester
       flash[:error] = "A semester has not been created or started yet. Please wait until the semester starts or create a new semester"
+      redirect_to error_path
     elsif not cur_semester.matchings_performed
       redirect_to new_matching_path
     end

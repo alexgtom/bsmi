89e6ec47013064e52bef29c65a9de192aaca6700
Alex Tom <alexgtom@berkeley.edu>
2013-01-18 00:57:27 -0800

    Fixed timeslot selections from deleting each other

--- app/controllers/select_timeslots_controller.rb
+++ app/controllers/select_timeslots_controller.rb
@@ -111,8 +111,10 @@ class SelectTimeslotsController < ApplicationController
         from_form = []
 
         Preference.find_by_student_id_and_cal_course_id(@student.id, @cal_course.id).each do |p|
+          if p.timeslot.day == step
             current << p.id
-        end
+          end
+         end
         if params[step]
           params[step].each do |timeslot_id|
             p = Preference.find_by_student_id_and_timeslot_id(@student.id, timeslot_id)

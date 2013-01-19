bda4a61159641c8c2e548019149488ed7b1f971e
Alex Tom <alexgtom@berkeley.edu>
2013-01-19 13:59:23 -0800

    Precomiled assets

--- public/assets/application.js
+++ public/assets/application.js
@@ -25408,29 +25408,6 @@ $.widget( "ui.tooltip", {
 
 
 }).call(this);
-RP = {
-  setup: function() {
-    $('#cal_course_school_type').change(RP.filter);
-    RP.filter();
-    //$('#entries_table tbody tr').each(function() { $(this).find('td:nth-child(2)').hide(); $(this).find('td:nth-child(2)').hide();});
-  },
-
-  filter: function() {
-    if ($('#entries_table')) {
-      $('#entries_table tbody tr:not(:first-child)').each(RP.filter_single_entry);
-    }
-  },
-
-  filter_single_entry: function(params) {
-    var type = $('#cal_course_school_type').val();
-    if ( type == "All" || type == $(this).find('td:nth-child(2)').text() ) {
-      $(this).show();
-    } else {
-      $(this).hide();
-    };
-  }
-}
-$(RP.setup);
 (function() {
 
 

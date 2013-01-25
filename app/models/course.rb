039f142401eaa04b2f4a24577547c4f6dbf5766a
Alex Tom <alexgtom@berkeley.edu>
2013-01-24 18:41:22 -0800

    Added K-5 and 6-8 option for grade level on course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   has_many :timeslots
   has_many :mentor_teachers, :through => :timeslots
 
-  GRADE = ["K", "1", "2", "3", "4", "5", "6", "7", "8", HIGH_SCHOOL]
+  GRADE = ["K", "1", "2", "3", "4", "5", "6", "7", "8", "K-5", "6-8", HIGH_SCHOOL]
   
   validates_presence_of :name, :grade
   attr_protected #none
